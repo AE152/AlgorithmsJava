de288c52562f16075a278358a20731c1aa8593a1
”Aleksandr <a.hartov152@mail.ru>
2024-04-03 21:02:00 +0700

    Fibonachi recursive + effective

--- Fibonachi/src/Main.java
+++ Fibonachi/src/Main.java
@@ -2,14 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println(fibonachi_recursive(10) + " "+ fibonachi_effective(22));
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static long fibonachi_recursive(int n){
+        if (n == 0){
+            return 0;
         }
+        if (n == 1){
+            return 1;
+        }
+        return fibonachi_recursive(n - 1) + fibonachi_recursive(n - 2);
+    }
+
+    public static long fibonachi_effective(int n){
+        long[] arr = new long[n+2];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i = 2; i <= n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
     }
 }
