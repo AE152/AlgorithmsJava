0014079cafec58e977f00e9e112a35581ab1fe77
”Aleksandr <a.hartov152@mail.ru>
2024-04-05 00:43:46 +0700

    add MinRefil - greedy Algorithm

--- GreedyAlgo/src/Main.java
+++ GreedyAlgo/src/Main.java
@@ -1,16 +1,13 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         int[] digits = {1, 5, 2, 3, 1, 9 ,5};
         System.out.println(MaxNumber(digits));
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
+        int[] stations = {0, 50, 250, 600, 700, 1000};
+        int reservePower = 400;
+        System.out.println(MinRefil(stations, reservePower));
 
     }
 
@@ -22,5 +19,23 @@ public class Main {
             res += digits[i];
         }
         return res;
+    }
+
+    //Возвращает минимально необходимое количество заправок для поездки
+    public static int MinRefil(int[] stations, int reservePower){
+        int currentState = 0;
+        int nextState = 0;
+        int result = 0;
+        while(currentState < stations.length - 1 ){
+
+            while(nextState < stations.length - 1 &&
+                    stations[nextState + 1] - stations[currentState] <= reservePower){
+                nextState++;
+            }
+            if(nextState != stations.length - 1)
+                result++;
+            currentState = nextState;
+        }
+        return result;
     }
 }
